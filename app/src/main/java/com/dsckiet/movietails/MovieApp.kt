11ae7660562f857294fb0de64eb00141e5b0969a
Anshul1507 <ag1507anshul@gmail.com>
2020-06-24 05:20:58 +0530

    koin injection --success network base call

---
+++ app/src/main/java/com/dsckiet/movietails/MovieApp.kt
@@ -0,0 +1,26 @@
+package com.dsckiet.movietails
+
+import android.app.Application
+import com.dsckiet.movietails.di.networkModule
+import com.dsckiet.movietails.di.viewModelModule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.InternalCoroutinesApi
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+@FlowPreview
+@ExperimentalCoroutinesApi
+@InternalCoroutinesApi
+class MovieApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        runKoin()
+    }
+    private fun runKoin() {
+        startKoin{
+            androidContext(applicationContext)
+            modules(networkModule, viewModelModule)
+        }
+    }
+}
