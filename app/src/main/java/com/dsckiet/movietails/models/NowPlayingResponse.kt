7fe0d3103566b6bf4581df8e2ecf3cb2827ba5b7
Anshul1507 <ag1507anshul@gmail.com>
2020-06-24 05:02:40 +0530

    added models --now playing

---
+++ app/src/main/java/com/dsckiet/movietails/models/NowPlayingResponse.kt
@@ -0,0 +1,23 @@
+package com.dsckiet.movietails.models
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class NowPlayingResponse(
+    @Json(name = "results")
+    val nowPlayingMoviesList: List<NowPlayingMovieModel>
+) : Parcelable
+
+@Parcelize
+data class NowPlayingMovieModel(
+
+    @Json(name = "vote_average") val nowPlayingMovieRating: String = "0",
+    @Json(name = "id") val nowPlayingMovieID : String = "0",
+    @Json(name = "poster_path") val nowPlayingMovieImageURL: String = "",
+    @Json(name = "original_title") val nowPlayingMovieTitle: String = "",
+    @Json(name = "release_date") val nowPlayingMovieReleaseDate: String = ""
+
+) : Parcelable
+
