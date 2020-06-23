aa938dcb15dbadb07dbaa6eda6958adfe72573ef
Anshul1507 <ag1507anshul@gmail.com>
2020-06-24 05:16:09 +0530

    added viewModel -- now playing

--- app/src/main/java/com/dsckiet/movietails/ui/home/MainActivity.kt
+++ app/src/main/java/com/dsckiet/movietails/ui/home/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.dsckiet.movietails
+package com.dsckiet.movietails.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.dsckiet.movietails.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
