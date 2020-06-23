4926759a14715e4aa9991ba63ba3034ed62fbb08
Anshul1507 <ag1507anshul@gmail.com>
2020-06-24 05:12:21 +0530

    inject offline data caching

--- app/src/main/java/com/dsckiet/movietails/di/NetworkModule.kt
+++ app/src/main/java/com/dsckiet/movietails/di/NetworkModule.kt
@@ -1,12 +1,17 @@
 package com.dsckiet.movietails.di
 
+import android.content.Context
 import com.dsckiet.movietails.network.TMDBApiService
 import com.dsckiet.movietails.network.TMDBApiService.Companion.TMDB_BASE_URL
 import com.dsckiet.movietails.repository.MovieDataRepository
+import com.dsckiet.movietails.utils.isNetworkAvailable
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -22,10 +27,31 @@ val networkModule = module {
                     Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
                 )
             )
+            .client(okHttpClient(androidContext()))
             .build()
             .create(TMDBApiService::class.java)
     }
     single {
         MovieDataRepository(get())
     }
+}
+
+fun okHttpClient(ctx: Context): OkHttpClient {
+    val cacheSize = (5 * 1024 * 1024).toLong()
+    val myCache = Cache(ctx.cacheDir, cacheSize)
+
+    return OkHttpClient.Builder()
+        .cache(myCache)
+        .addInterceptor { chain ->
+            var request = chain.request()
+            request = if (isNetworkAvailable(ctx)!!)
+                request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+            else
+                request.newBuilder().header(
+                    "Cache-Control",
+                    "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7
+                ).build()
+            chain.proceed(request)
+        }
+        .build()
 }
